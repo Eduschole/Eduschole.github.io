234948d64e9da93065d9fa8fb43de56b4d09b3ce
Alejandro M. Moreno <alejandromorenomuller@gmail.com>
2024-01-14 21:24:09 -0600

    Add hide camera functionality and mute mic functionality

--- main.js
+++ main.js
@@ -165,6 +165,35 @@ let leaveChannel = async () => {
     await client.logout()
 }
 
+let toggleCamera = async () => {
+    let videoTrack = localStream.getTracks().find(track => track.kind === 'video')
+    if (videoTrack.enabled){
+        videoTrack.enabled = false
+        document.getElementById('camera-btn').style.backgroundColor= 'rgb(255, 80, 80)'
+    }
+    else {
+        videoTrack.enabled = true
+        document.getElementById('camera-btn').style.backgroundColor= 'rgb(179, 102, 249)'
+
+    }
+}
+
+let toggleMic = async () => {
+    let audioTrack = localStream.getTracks().find(track => track.kind === 'audio')
+    if (audioTrack.enabled){
+        audioTrack.enabled = false
+        document.getElementById('mic-btn').style.backgroundColor= 'rgb(255, 80, 80)'
+    }
+    else {
+        audioTrack.enabled = true
+        document.getElementById('mic-btn').style.backgroundColor= 'rgb(179, 102, 249)'
+
+    }
+}
+
 // before the window is closed, run leave channel
 window.addEventListener('beforeunload',leaveChannel);
+
+document.getElementById('camera-btn').addEventListener('click',toggleCamera)
+document.getElementById('mic-btn').addEventListener('click',toggleMic)
 init();
